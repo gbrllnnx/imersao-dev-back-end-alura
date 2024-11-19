07e9ea7a67c51390a17331e59e186495e5c938d7
gbrllnnx <gbrllnnx@gmail.com>
2024-11-19 17:46:52 -0300

    cria rota POSTS

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 import express from 'express';
 
+const posts = [
+  {
+    descricao: 'uma foto teste',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    descricao: 'Gato brincando com bola',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    descricao: 'Paisagem montanhosa',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    descricao: 'Cachorro correndo na praia',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    descricao: 'Comida deliciosa',
+    imagem: 'https://placecats.com/millie/300/150'
+  },
+  {
+    descricao: 'Cidades futuristas',
+    imagem: 'https://placecats.com/millie/300/150'
+  }
+];
+
 const app = express();
 const port = 3000;
+
+// recebe texto e converte em JSON manipulavel
+app.use(express.json());
 
 app.listen(port, () => {
   console.log('Servidor online');
 });
 
-app.get('/api', (req, res) => {
-  res.status(200).send('Hello Mario World!');
+app.get('/posts', (req, res) => {
+  res.status(200).json(posts);
 });
 
+// Rota alternativa sugerida pela Luri
 app.get('/livro', (req, res) => {
   const livro = {
     "titulo": "O Senhor dos Anéis",
